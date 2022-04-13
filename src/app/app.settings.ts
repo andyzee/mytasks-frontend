11fbde8fdfcd3d73a67332a9f9d1ad6b6e295fae
andyzee <andreyzyabkin@gmail.com>
2022-04-14 01:41:22 +0300

    switchet isDevMode to enviroment

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,5 +1,6 @@
-import { isDevMode } from "@angular/core";
+// import { isDevMode } from "@angular/core";
+import { environment } from "../environments/environment"
 
 export class AppSettings {
-  static apiUrl: string = isDevMode() ? 'http://127.0.0.1:3000' : 'https://murmuring-peak-08274.herokuapp.com'
+  static apiUrl: string = environment.production ? 'https://murmuring-peak-08274.herokuapp.com' : 'http://127.0.0.1:3000'
 }
