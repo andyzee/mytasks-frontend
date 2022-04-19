ca931adedb5a70b04df2288f169fc3c94999066f
andyzee <andreyzyabkin@gmail.com>
2022-04-19 22:48:13 +0300

    fork to eu region

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -2,5 +2,5 @@
 import { environment } from "../environments/environment"
 
 export class AppSettings {
-  static apiUrl: string = environment.production ? 'https://murmuring-peak-08274.herokuapp.com' : 'http://127.0.0.1:3000'
+  static apiUrl: string = environment.production ? 'https://mytasks-042022.herokuapp.com/' : 'http://127.0.0.1:3000'
 }
